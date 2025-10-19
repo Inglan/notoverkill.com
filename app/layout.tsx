b3f5a4cadf61d363614524150c2fd2045f9ca278
Ingo Wolf <me@ingowolf.au>
2025-10-19 12:38:06 +1100

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="w-full border-b">
+          <div className="container mx-auto p-2 flex flex-row">
+            <Image src="/notoverkill.svg" alt="Logo" width={100} height={100} />
+            <div className="grow"></div>
+            <Button>Log in</Button>
+          </div>
+        </div>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
