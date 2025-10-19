638b92d009aec0487c3f4eaac4a8b1ec3689d81a
Ingo Wolf <me@ingowolf.au>
2025-10-19 12:46:46 +1100

    change hero a bit

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="h-96 flex justify-center items-center flex-col">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-4xl">My homelab is not overkill</h1>
-          <h2 className="text-xl">Not even a bit</h2>
-        </div>
+      <div className="h-96 flex justify-center items-center flex-col gap-2">
+        <h1 className="text-4xl">My homelab is not overkill</h1>
+        <h2 className="text-xl">Not even a bit</h2>
       </div>
     </>
   );
