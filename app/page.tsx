eef349e9e7cae28fde1589d77c94b94e96544524
Ingo Wolf <me@ingowolf.au>
2025-10-19 13:28:20 +1100

    add more padding

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <main className="container mx-auto">
-        <div className="h-96 flex justify-center flex-col gap-4 p-2 max-w-4xl mx-auto">
+        <div className="h-96 flex justify-center flex-col gap-4 p-4 max-w-4xl mx-auto">
           <h1 className="text-4xl md:text-6xl">
             My homelab
             <br />
@@ -12,7 +12,7 @@ export default function Home() {
           </h1>
           <h2 className="text-xl">Not even a bit</h2>
         </div>
-        <div className="p-2 flex flex-col gap-2 max-w-4xl mx-auto">
+        <div className="p-4 flex flex-col gap-2 max-w-4xl mx-auto">
           <h2 className="text-2xl">
             If you don't believe me, look at these setups:
           </h2>
