139f3b57149079d8b2fd59a27326db8599f713b5
Ingo Wolf <me@ingowolf.au>
2025-10-19 15:44:20 +1100

    change navbar

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="w-full border-b">
+    <div className="w-full border-b bg-background/50 sticky top-0 backdrop-blur-xl">
       <div className="container mx-auto p-2 flex flex-row">
         <Image src="/notoverkill.svg" alt="Logo" width={100} height={100} />
         <div className="grow"></div>
