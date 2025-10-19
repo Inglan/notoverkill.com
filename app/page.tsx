3344c349fc5b1251f7a479c3af250ff006687cee
Ingo Wolf <me@ingowolf.au>
2025-10-19 13:24:36 +1100

    do some layout stuiff

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,21 @@ import HomelabScroller from "@/components/homelabscroller";
 export default function Home() {
   return (
     <>
-      <div className="h-96 flex justify-center items-center flex-col gap-2 p-2">
-        <h1 className="text-4xl">My homelab is not overkill</h1>
-        <h2 className="text-xl">Not even a bit</h2>
-      </div>
-      <div className="container mx-auto p-2 flex flex-col gap-2">
-        <h2 className="text-2xl">
-          If you don't believe me, look at these setups:
-        </h2>
-        <HomelabScroller />
-        <div className="text-foreground/50 text-sm">From r/homelab</div>
-      </div>
+      <main className="container mx-auto">
+        <div className="h-96 flex justify-center flex-col gap-2 p-2 max-w-4xl mx-auto">
+          <h1 className="text-4xl">
+            My homelab is <br /> not overkill
+          </h1>
+          <h2 className="text-xl">Not even a bit</h2>
+        </div>
+        <div className="p-2 flex flex-col gap-2 max-w-4xl mx-auto">
+          <h2 className="text-2xl">
+            If you don't believe me, look at these setups:
+          </h2>
+          <HomelabScroller />
+          <div className="text-foreground/50 text-sm">From r/homelab</div>
+        </div>
+      </main>
     </>
   );
 }
