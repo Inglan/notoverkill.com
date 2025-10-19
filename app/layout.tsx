6ef7ca0ef7d47f8d5c6bbc59e6ecb43dbc8b2023
Ingo Wolf <me@ingowolf.au>
2025-10-19 12:45:36 +1100

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -26,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistMono.className} antialiased`}>
         <Header />
         <ThemeProvider
           attribute="class"
