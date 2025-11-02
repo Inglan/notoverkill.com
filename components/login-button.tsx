80e50391f38b2bc4ffef46a0a9e9c96541cf9bdc
Ingo Wolf <me@ingowolf.au>
2025-11-02 12:55:43 +1100

    add some conditions to login button

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -2,10 +2,15 @@
 
 import { authClient } from "@/lib/auth-client";
 import { Button } from "./ui/button";
+import { useConvexAuth } from "convex/react";
+import { UserButton } from "@daveyplate/better-auth-ui";
 
 export default function LoginButton() {
+  const convexAuth = useConvexAuth();
+  if (convexAuth.isAuthenticated) return <UserButton size="icon" />;
   return (
     <Button
+      disabled={convexAuth.isLoading || convexAuth.isAuthenticated}
       onClick={() => {
         authClient.signIn.oauth2({
           providerId: "notoverkill",
