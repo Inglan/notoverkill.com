efe926ab13b0d23691263e95a2074bc146aca0a9
Ingo Wolf <me@ingowolf.au>
2025-11-18 17:03:51 +1100

    add user button when logged in

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -2,19 +2,27 @@
 
 import { authClient } from "@/lib/auth-client";
 import { Button } from "./ui/button";
+import { SignedIn, SignedOut, UserButton } from "@daveyplate/better-auth-ui";
 
 export default function LoginButton() {
   // if (convexAuth.isAuthenticated) return <UserButton size="icon" />;
   return (
-    <Button
-      // disabled={convexAuth.isLoading || convexAuth.isAuthenticated}
-      onClick={() => {
-        authClient.signIn.oauth2({
-          providerId: "notoverkill",
-        });
-      }}
-    >
-      Log in
-    </Button>
+    <>
+      <SignedOut>
+        <Button
+          // disabled={convexAuth.isLoading || convexAuth.isAuthenticated}
+          onClick={() => {
+            authClient.signIn.oauth2({
+              providerId: "notoverkill",
+            });
+          }}
+        >
+          Log in
+        </Button>
+      </SignedOut>
+      <SignedIn>
+        <UserButton size="sm" variant="ghost" />
+      </SignedIn>
+    </>
   );
 }
