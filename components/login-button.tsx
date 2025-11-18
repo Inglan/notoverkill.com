f4e29c44de8c5ebb2b64cf7fbab784faa8d06f59
Ingo Wolf <me@ingowolf.au>
2025-11-19 09:14:21 +1100

    update login button

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -2,16 +2,21 @@
 
 import { authClient } from "@/lib/auth-client";
 import { Button } from "./ui/button";
-import { SignedIn, SignedOut, UserButton } from "@daveyplate/better-auth-ui";
+import { SignedIn, UserButton } from "@daveyplate/better-auth-ui";
+import { Skeleton } from "./ui/skeleton";
+import { useState } from "react";
 
 export default function LoginButton() {
-  // if (convexAuth.isAuthenticated) return <UserButton size="icon" />;
+  const session = authClient.useSession();
+  const [loading, setLoading] = useState(false);
+
   return (
     <>
-      <SignedOut>
+      {!session.data?.session && (
         <Button
-          // disabled={convexAuth.isLoading || convexAuth.isAuthenticated}
+          disabled={session.isPending || loading}
           onClick={() => {
+            setLoading(true);
             authClient.signIn.oauth2({
               providerId: "notoverkill",
             });
@@ -19,7 +24,7 @@ export default function LoginButton() {
         >
           Log in
         </Button>
-      </SignedOut>
+      )}
       <SignedIn>
         <UserButton size="sm" variant="ghost" />
       </SignedIn>
