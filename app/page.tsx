3e88137bed97514d7ba888823784501e6016552e
Ingo Wolf <me@ingowolf.au>
2025-11-18 17:56:55 +1100

    add dynamically loading different pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import { auth } from "@/lib/auth";
 import Home from "./home/page";
+import { headers } from "next/headers";
+import AppsPage from "./apps/page";
 
-export default Home;
+export default async function Root() {
+  const headersList = await headers();
+  const session = await auth.api.getSession({ headers: headersList });
+
+  if (!session) {
+    return Home();
+  } else {
+    return AppsPage();
+  }
+}
