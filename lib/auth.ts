427704d8a01b6ad496e3897ae07082322a0d2045
Ingo Wolf <me@ingowolf.au>
2025-11-18 16:55:52 +1100

    change to use oatuh

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
+import { genericOAuth } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: new Pool({
@@ -9,6 +10,19 @@ export const auth = betterAuth({
     },
   }),
   emailAndPassword: {
-    enabled: true,
+    enabled: false,
   },
+  plugins: [
+    genericOAuth({
+      config: [
+        {
+          providerId: "notoverkill",
+          clientId: process.env.CLIENT_ID!,
+          clientSecret: process.env.CLIENT_SECRET!,
+          discoveryUrl: process.env.DISCOVERY_URL!,
+          scopes: ["openid", "email", "profile", "goauthentik.io/api"],
+        },
+      ],
+    }),
+  ],
 });
