a239121d15f53e3f3d523b83993e1f45491014d6
Ingo Wolf <me@ingowolf.au>
2025-11-19 08:58:40 +1100

    hide notoverkill.com

--- components/apps-page.tsx
+++ components/apps-page.tsx
@@ -18,18 +18,20 @@ export default async function AppsPage() {
 
   const coreApi = new CoreApi(config);
 
-  const applications = await coreApi.coreApplicationsList({
-    onlyWithLaunchUrl: true,
-    ordering: "name",
-    page: 1,
-    pageSize: 10000,
-  });
+  const applications = (
+    await coreApi.coreApplicationsList({
+      onlyWithLaunchUrl: true,
+      ordering: "name",
+      page: 1,
+      pageSize: 10000,
+    })
+  ).results.filter((app) => app.name != "notoverkill.com");
 
   return (
     <div className="w-full max-w-4xl mx-auto p-2 flex flex-col gap-2">
       <h1 className="text-4xl">Apps</h1>
       <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2">
-        {applications.results.map((app) => (
+        {applications.map((app) => (
           <Link
             className="flex flex-col items-center justify-center p-4 bg-card rounded border text-center gap-2"
             key={app.pk}
