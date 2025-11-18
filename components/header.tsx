c6bb8a8f85d4861689911076d12a9234d617f085
Ingo Wolf <me@ingowolf.au>
2025-11-19 09:04:08 +1100

    change navbar styles

--- components/header.tsx
+++ components/header.tsx
@@ -5,20 +5,18 @@ import LoginButton from "./login-button";
 export default function Header() {
   return (
     <div className="w-full border-b bg-background/50 sticky top-0 backdrop-blur-xl z-50">
-      <div className="container mx-auto">
-        <nav className="max-w-4xl mx-auto h-15 flex flex-row items-center">
-          <Link href="/">
-            <Image
-              src="/notoverkill.svg"
-              alt="Logo"
-              width={275 / 2}
-              height={76 / 2}
-            />
-          </Link>
-          <div className="grow"></div>
-          <LoginButton />
-        </nav>
-      </div>
+      <nav className="max-w-4xl mx-auto h-15 flex flex-row items-center p-4">
+        <Link href="/">
+          <Image
+            src="/notoverkill.svg"
+            alt="Logo"
+            width={275 / 2}
+            height={76 / 2}
+          />
+        </Link>
+        <div className="grow"></div>
+        <LoginButton />
+      </nav>
     </div>
   );
 }
