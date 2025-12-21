9e36aa4944f7a2b0be0bbce7db1c16842536985e
Ingo Wolf <me@ingowolf.au>
2025-12-21 20:38:16 +1100

    add plugins

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,2 +1,7 @@
+import { magicLinkClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-export const authClient = createAuthClient({});
+import { passkeyClient } from "@better-auth/passkey/client";
+
+export const authClient = createAuthClient({
+  plugins: [magicLinkClient(), passkeyClient()],
+});
