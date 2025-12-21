f31071e257373ba3122e081225585f37b35be81e
Ingo Wolf <me@ingowolf.au>
2025-12-21 20:13:31 +1100

    delete everything

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL(process.env.AUTHENTIK_SERVER + "/**")],
-  },
-};
-
-export default nextConfig;
