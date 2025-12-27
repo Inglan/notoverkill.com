4ddb7915689c077b4cf3c44d0efdce878b675e3a
Ingo Wolf <me@ingowolf.au>
2025-12-27 18:27:32 +1100

    add aniamte in

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -11,13 +11,18 @@ import { useEffect, useState } from "react";
 import { authClient } from "@/lib/auth-client";
 import { Spinner } from "@/components/ui/spinner";
 import { toast } from "sonner";
+import { motion } from "motion/react";
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm bg-card border rounded-lg flex flex-col p-4 gap-4">
+      <motion.div
+        initial={{ scale: 0.9, opacity: 0, y: 20 }}
+        animate={{ scale: 1, opacity: 1, y: 0 }}
+        className="w-full max-w-sm bg-card border rounded-lg flex flex-col p-4 gap-4"
+      >
         <Form />
-      </div>
+      </motion.div>
     </div>
   );
 }
