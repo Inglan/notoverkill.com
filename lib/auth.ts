3af0785cc6c7281ba1caf6665ad8e146861bdd25
Ingo Wolf <me@ingowolf.au>
2025-12-27 17:38:33 +1100

    add google and github providers

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,4 +18,14 @@ export const auth = betterAuth({
     }),
     passkey(),
   ],
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
 });
