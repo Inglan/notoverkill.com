b601db7c402a24532cc3bc24fecd57cabf4206cb
Ingo Wolf <me@ingowolf.au>
2025-12-27 17:23:06 +1100

    add container for login

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,3 +1,7 @@
 export default function SignInPage() {
-  return <>e</>;
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm bg-card border rounded-lg">e</div>
+    </div>
+  );
 }
