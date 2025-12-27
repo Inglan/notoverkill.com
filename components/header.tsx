33b8ba392adeee5272d9c846c0b584bf55afd057
Ingo Wolf <me@ingowolf.au>
2025-12-27 17:18:19 +1100

    add header auth section

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import HeaderAuth from "./header-auth";
 
 export default function Header() {
   return (
@@ -13,6 +14,8 @@ export default function Header() {
             height={76 / 2}
           />
         </Link>
+        <div className="grow"></div>
+        <HeaderAuth />
       </nav>
     </div>
   );
