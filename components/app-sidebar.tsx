139d0c12645d197642270d2be6b1cacfeeab6924
Ingo Wolf <me@ingowolf.au>
2025-12-29 15:00:21 +1100

    add menu items

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -4,15 +4,41 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Home } from "lucide-react";
+import Link from "next/link";
+
+const items: {
+  name: string;
+  href: string;
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+}[] = [
+  {
+    name: "Home",
+    href: "/",
+    icon: Home,
+  },
+];
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.href}>
+              <SidebarMenuButton asChild>
+                <Link href={item.href}>
+                  <item.icon />
+                  {item.name}
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
