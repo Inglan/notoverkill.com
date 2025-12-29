4383e5870bf3f315e380ecc94c01a644b4161b08
Ingo Wolf <me@ingowolf.au>
2025-12-29 14:20:55 +1100

    redirect if already signed in

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,11 +1,22 @@
+import { auth } from "@/lib/auth";
 import Form from "./form";
 import { Metadata } from "next";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const headersList = await headers();
+
+  const session = await auth.api.getSession({
+    headers: headersList,
+  });
+
+  if (session) redirect("/");
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <Form />
